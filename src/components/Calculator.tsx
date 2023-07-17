3362e578f85c0372fa6869bb0f9e834e3be52155
Leonardo Marques <dev.work.leo@gmail.com>
2023-07-16 21:53:59 -0300

    create calculator component

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,3 +1,27 @@
+import { CalculatorButtons, CalculatorFlutter, CalculatorLayoyt, Dot, Dots, DotsAndTheme } from "../global"
+import { MainButtons } from "./MainButtons"
+import { MathArea } from "./MathArea"
+import { Switch } from 'antd';
+import { useState } from "react"
+
 export const Calculator = () => {
-  return <>Calculadora</>
+  const [isDark, setIsDark] = useState(true)
+  return <CalculatorLayoyt>
+    <CalculatorFlutter background={isDark ? '#212B41' : '#F4FDFB'} >
+      <DotsAndTheme>
+        <Dots>
+          { Array.from({ length: 3 }, (_, index: number) => <Dot key={index} />) }
+        </Dots>
+        <Switch defaultChecked onChange={() => setIsDark(!isDark)} className={isDark ? 'dark_switch': 'light_switch'} />
+      </DotsAndTheme>
+      <MathArea background={isDark ? '#212B41' : '#F4FDFB'} />
+      <CalculatorButtons background={isDark ? '#2E3951' : '#FFFFFF'}>
+        <MainButtons
+          isDark={isDark}
+          color={isDark ? '#F4FDFB ' : '#373E47'}
+          background={isDark ? '#2E3951' : '#F4FDFB'}
+        />
+      </CalculatorButtons>
+    </CalculatorFlutter>
+  </CalculatorLayoyt>
 }
