a422dff06dcbbde0822ebaa9065a1bbd8ae6f486
Leonardo Marques <dev.work.leo@gmail.com>
2023-07-17 11:41:10 -0300

    remove unnecessary component

--- src/components/SideButtons.tsx
+++
@@ -1,8 +0,0 @@
-import { ButtonSectionLayout } from "../global"
-
-export const SideButtons = ({ color, background }:{ color: string, background: string }) => {
-  console.log(color, background)
-  return <ButtonSectionLayout>
-    Botões laterais
-  </ButtonSectionLayout>
-}
