f0cb7c8ff6a07064aee6024d0f1a5c06d61c9a46
Leonardo Marques <dev.work.leo@gmail.com>
2023-07-17 11:40:50 -0300

    update colors from dark and light themes

--- src/components/MainButtons.tsx
+++ src/components/MainButtons.tsx
@@ -4,36 +4,37 @@ import { FaSquareRootAlt, FaDivide, FaCircle } from "react-icons/fa"
 import { RxReload } from "react-icons/rx"
 
 export const MainButtons = ({ isDark, color, background }:{ isDark: boolean, color: string, background: string }) => {
+  const primaryButton = isDark ? 'primary_button_dark' : 'primary_button_light'
   const items = [
     { id: 1, buttons: [
-      { label: <>C</>, theme: { className: 'green_button' } },
-      { label: <FaSquareRootAlt />, theme: { className: 'green_button' } },
-      { label: <PercentageOutlined />, theme: { className: 'green_button' } },
-      { label: <FaDivide />, theme: { className: 'green_button' } }
+      { label: <>C</>, theme: { className: primaryButton } },
+      { label: <FaSquareRootAlt />, theme: { className: primaryButton } },
+      { label: <PercentageOutlined />, theme: { className: primaryButton } },
+      { label: <FaDivide />, theme: { className: primaryButton } }
     ] },
     { id: 2, buttons: [
       { label: <Number>7</Number> },
       { label: <Number>8</Number> },
       { label: <Number>9</Number> },
-      { label: <CloseOutlined />, theme: { className: 'green_button' } }
+      { label: <CloseOutlined />, theme: { className: primaryButton } }
     ] },
     { id: 3, buttons: [
       { label: <Number>4</Number> },
       { label: <Number>5</Number>},
       { label: <Number>6</Number> },
-      { label: <MinusOutlined />, theme: { className: 'green_button' } }
+      { label: <MinusOutlined />, theme: { className: primaryButton } }
     ] },
     { id: 4, buttons: [
       { label: <Number>1</Number> },
       { label: <Number>2</Number> },
       { label: <Number>3</Number> },
-      { label: <PlusOutlined />, theme: { className: 'green_button' } }
+      { label: <PlusOutlined />, theme: { className: primaryButton } }
     ] },
     { id: 5, buttons: [
       { label: <RxReload className='rxdeg' /> },
       { label: <>0</> },
       { label: <FaCircle className="circle" /> },
-      { label: <PauseOutlined className="rotate" />, theme: { className: 'green_button' } }
+      { label: <PauseOutlined className="rotate" />, theme: { className: primaryButton } }
     ] },
   ]
   return <ButtonSectionLayout>
