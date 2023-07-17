2373e4a209b476dccc3debde2ec4272bed089a3c
Leonardo Marques <dev.work.leo@gmail.com>
2023-07-16 21:52:25 -0300

    create math area component

---
+++ src/components/MathArea.tsx
@@ -0,0 +1,7 @@
+import { MathAreaLayout } from "../global"
+
+export const MathArea = ({ background }:{ background: string }) => {
+  return <MathAreaLayout background={background}>
+
+  </MathAreaLayout>
+}
